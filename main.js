524c4e775acea932b966daf4ba87450897571363
Prisberg <fille.risberg@live.se>
2023-01-31 08:47:10 +0100

    tilt output

---
+++ main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  windowListener();
+};
+
+function windowListener() {
+  if ("DeviceOrientationEvent" in window) {
+    window.addEventListener("deviceorientation", function (event) {
+      let beta = event.beta;
+      let gamma = event.gamma;
+      renderText(`Tilt output: ${beta}, ${gamma}`);
+    });
+  } else {
+    renderText(`DeviceOrientationEvent is not supported`);
+  }
+}
+
+function renderText(text) {
+  let output = document.createElement("div");
+  output.innerText = text;
+  document.body.appendChild(output);
+}
