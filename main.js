bb604757d1a5490cf72e00ac48d3ef10f8137bcd
Prisberg <fille.risberg@live.se>
2023-02-02 10:13:31 +0100

    conditional gyro

--- main.js
+++ main.js
@@ -1,41 +1,15 @@
-// NOTE: game must be hosted on HTTPS site to access DeviceOrientationEvent.
+// NOTE: game must be hosted on HTTPS site to access DeviceOrientationEvent in iOS.
 
 window.onload = function () {
   // windowListener();
   const button = document.getElementById("button");
   console.log(button);
-  button.addEventListener("click", getAccel);
+  button.addEventListener("click", getPermission);
 };
 
-// function windowListener() {
-//   if ("DeviceOrientationEvent" in window) {
-//     DeviceOrientationEvent.requestPermission().then((response) => {
-//       if (response === "granted") {
-//         window.addEventListener("deviceOrientationEvent", function (event) {
-//           let beta = event.beta;
-//           let gamma = event.gamma;
-//           renderText(`Tilt output: ${beta}, ${gamma}`);
-//         });
-//       }
-//     });
-//   } else {
-//     renderText(`DeviceOrientationEvent is not supported`);
-//   }
-// }
-
-// function renderText(text) {
-//   let output = document.getElementById("output");
-//   output.innerHTML = text;
-// }
-
 let logParagraph = document.getElementById("log");
-let px = 50; // Position x and y
-let py = 50;
-let vx = 0.0; // Velocity x and y
-let vy = 0.0;
-let updateRate = 1 / 60; // Sensor refresh rate
 
-function getAccel() {
+function getPermission() {
   if ("DeviceMotionEvent" in window) {
     logParagraph.innerHTML = "click";
 
@@ -45,36 +19,7 @@ function getAccel() {
           if (response == "granted") {
             // Add a listener to get smartphone orientation
             // in the alpha-beta-gamma axes (units in degrees)
-            window.addEventListener("deviceorientation", (event) => {
-              // Expose each orientation angle in a more readable way
-              rotation_degrees = event.alpha;
-              frontToBack_degrees = event.beta;
-              leftToRight_degrees = event.gamma;
-
-              // Update velocity according to how tilted the phone is
-              // Since phones are narrower than they are long, double the increase to the x velocity
-              vx = vx + leftToRight_degrees * updateRate * 2;
-              vy = vy + frontToBack_degrees * updateRate;
-
-              // Update position and clip it to bounds
-              px = px + vx * 0.5;
-              if (px > 98 || px < 0) {
-                px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
-                vx = 0;
-              }
-
-              py = py + vy * 0.5;
-              if (py > 98 || py < 0) {
-                py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
-                vy = 0;
-              }
-
-              dot = document.getElementsByClassName("indicatorDot")[0];
-              dot.setAttribute(
-                "style",
-                "left:" + px + "%;" + "top:" + py + "%;"
-              );
-            });
+            gyroListener();
           }
         })
         .catch((error) => {
@@ -84,8 +29,45 @@ function getAccel() {
     } else {
       logParagraph.innerHTML =
         "requestPermission in DeviceMotionEvent does not exist..";
+      gyroListener();
     }
   } else {
     logParagraph.innerHTML = "DeviceMotionEvent in window does not exist..";
   }
+}
+
+function gyroListener() {
+  let px = 50; // Position x and y
+  let py = 50;
+  let vx = 0.0; // Velocity x and y
+  let vy = 0.0;
+  let updateRate = 1 / 60; // Sensor refresh rate
+
+  window.addEventListener("deviceorientation", (event) => {
+    // Expose each orientation angle in a more readable way
+    rotation_degrees = event.alpha;
+    frontToBack_degrees = event.beta;
+    leftToRight_degrees = event.gamma;
+
+    // Update velocity according to how tilted the phone is
+    // Since phones are narrower than they are long, double the increase to the x velocity
+    vx = vx + leftToRight_degrees * updateRate * 2;
+    vy = vy + frontToBack_degrees * updateRate;
+
+    // Update position and clip it to bounds
+    px = px + vx * 0.5;
+    if (px > 98 || px < 0) {
+      px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
+      vx = 0;
+    }
+
+    py = py + vy * 0.5;
+    if (py > 98 || py < 0) {
+      py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
+      vy = 0;
+    }
+
+    dot = document.getElementsByClassName("indicatorDot")[0];
+    dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
+  });
 }
