440627304f5fe9d51d817964a3d96e039e5d1a68
Prisberg <fille.risberg@live.se>
2023-02-02 10:07:47 +0100

    working log🤦

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ window.onload = function () {
 //   output.innerHTML = text;
 // }
 
-const logParagraph = document.getElementById("log");
+let logParagraph = document.getElementById("log");
 let px = 50; // Position x and y
 let py = 50;
 let vx = 0.0; // Velocity x and y
@@ -37,46 +37,55 @@ let updateRate = 1 / 60; // Sensor refresh rate
 
 function getAccel() {
   if ("DeviceMotionEvent" in window) {
-    logParagraph.innerHTML("click");
+    logParagraph.innerHTML = "click";
 
-    DeviceMotionEvent.requestPermission()
-      .then((response) => {
-        if (response == "granted") {
-          // Add a listener to get smartphone orientation
-          // in the alpha-beta-gamma axes (units in degrees)
-          window.addEventListener("deviceorientation", (event) => {
-            // Expose each orientation angle in a more readable way
-            rotation_degrees = event.alpha;
-            frontToBack_degrees = event.beta;
-            leftToRight_degrees = event.gamma;
+    if ("requestPermission" in DeviceMotionEvent) {
+      DeviceMotionEvent.requestPermission()
+        .then((response) => {
+          if (response == "granted") {
+            // Add a listener to get smartphone orientation
+            // in the alpha-beta-gamma axes (units in degrees)
+            window.addEventListener("deviceorientation", (event) => {
+              // Expose each orientation angle in a more readable way
+              rotation_degrees = event.alpha;
+              frontToBack_degrees = event.beta;
+              leftToRight_degrees = event.gamma;
 
-            // Update velocity according to how tilted the phone is
-            // Since phones are narrower than they are long, double the increase to the x velocity
-            vx = vx + leftToRight_degrees * updateRate * 2;
-            vy = vy + frontToBack_degrees * updateRate;
+              // Update velocity according to how tilted the phone is
+              // Since phones are narrower than they are long, double the increase to the x velocity
+              vx = vx + leftToRight_degrees * updateRate * 2;
+              vy = vy + frontToBack_degrees * updateRate;
 
-            // Update position and clip it to bounds
-            px = px + vx * 0.5;
-            if (px > 98 || px < 0) {
-              px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
-              vx = 0;
-            }
+              // Update position and clip it to bounds
+              px = px + vx * 0.5;
+              if (px > 98 || px < 0) {
+                px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
+                vx = 0;
+              }
 
-            py = py + vy * 0.5;
-            if (py > 98 || py < 0) {
-              py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
-              vy = 0;
-            }
+              py = py + vy * 0.5;
+              if (py > 98 || py < 0) {
+                py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
+                vy = 0;
+              }
 
-            dot = document.getElementsByClassName("indicatorDot")[0];
-            dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+              dot = document.getElementsByClassName("indicatorDot")[0];
+              dot.setAttribute(
+                "style",
+                "left:" + px + "%;" + "top:" + py + "%;"
+              );
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          logParagraph.innerHTML = error;
+        });
+    } else {
+      logParagraph.innerHTML =
+        "requestPermission in DeviceMotionEvent does not exist..";
+    }
   } else {
-    logParagraph.innerHTML("DeviceMotionEvent in window does not exist..");
+    logParagraph.innerHTML = "DeviceMotionEvent in window does not exist..";
   }
 }
