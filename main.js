1bd509224a3d7d85ac2eb6255f9435ec393806bb
Prisberg <fille.risberg@live.se>
2023-02-06 08:09:51 +0100

    better logs

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-// NOTE: game must be hosted on HTTPS site to access DeviceOrientationEvent in iOS.
+// NOTE: game must be hosted on HTTPS site to access DeviceMotion and getPermission in iOS.
 
 window.onload = function () {
-  // windowListener();
   const button = document.getElementById("button");
-  console.log(button);
   button.addEventListener("click", getPermission);
+  // DeviceOrientationFunc();
 };
 
 let logParagraph = document.getElementById("log");
 
 function getPermission() {
   if ("DeviceMotionEvent" in window) {
-    logParagraph.innerHTML = "click";
+    logParagraph.innerHTML = "running getPermission";
+    console.log("running getPermission");
 
     if ("requestPermission" in DeviceMotionEvent) {
+      logParagraph.innerHTML = "getPermission exists";
+      console.log("getPermission exists");
       DeviceMotionEvent.requestPermission()
         .then((response) => {
           if (response == "granted") {
@@ -28,18 +30,20 @@ function getPermission() {
         });
     } else {
       //
+      console.log("requestPermission in DeviceMotionEvent does not exist..");
       logParagraph.innerHTML =
         "requestPermission in DeviceMotionEvent does not exist..";
       gyroListener();
     }
   } else {
     logParagraph.innerHTML = "DeviceMotionEvent in window does not exist..";
+    console.log("DeviceMotionEvent in window does not exist..");
   }
 }
 
 function gyroListener() {
   let px = 50; // Position x and y
-  let py = 50;
+  // let py = 50;
   let vx = 0.0; // Velocity x and y
   let vy = 0.0;
   let updateRate = 1 / 60; // Sensor refresh rate
@@ -47,13 +51,15 @@ function gyroListener() {
   window.addEventListener("devicemotion", (event) => {
     // Expose each orientation angle in a more readable way
     rotation_degrees = event.alpha;
-    frontToBack_degrees = event.beta;
-    leftToRight_degrees = event.gamma;
+    beta = event.beta;
+    gamma = event.gamma;
+    console.log(event);
+    logParagraph.innerHTML = `Tilt output: ${beta}, ${gamma}`;
 
     // Update velocity according to how tilted the phone is
     // Since phones are narrower than they are long, double the increase to the x velocity
-    vx = vx + leftToRight_degrees * updateRate * 2;
-    vy = vy + frontToBack_degrees * updateRate;
+    vx = vx + gamma * updateRate * 2;
+    vy = vy + beta * updateRate;
 
     // Update position and clip it to bounds
     px = px + vx * 0.5;
@@ -72,3 +78,17 @@ function gyroListener() {
     dot.setAttribute("style", "left:" + px + "%;");
   });
 }
+
+// function DeviceOrientationFunc() {
+//   logParagraph.innerHTML = "running";
+//   if ("DeviceMotionEvent" in window) {
+//     window.addEventListener("devicemotion", function (event) {
+//       console.log(event);
+//       let beta = event.beta;
+//       let gamma = event.gamma;
+//       logParagraph.innerHTML = `Tilt output: ${beta}, ${gamma}`;
+//     });
+//   } else {
+//     logParagraph.innerHTML = `DeviceMotionEvent is not supported`;
+//   }
+// }
